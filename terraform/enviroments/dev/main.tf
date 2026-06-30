4a6f430c09340bd61814b443470ad30d3e68b52f
LeemaJosephine <leemajosephine3@gmail.com>
2026-07-01 00:39:59 +0530

    Add ECR module

--- terraform/enviroments/dev/main.tf
+++ terraform/enviroments/dev/main.tf
@@ -8,3 +8,9 @@ module "vpc" {
   private_subnet_cidrs = var.private_subnet_cidrs
   availability_zones   = var.availability_zones
 }
+
+module "ecr" {
+  source = "../../modules/ecr"
+
+  environment = var.environment
+}
