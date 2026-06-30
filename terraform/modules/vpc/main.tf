bb73775aed20b6f69bfd75cefecebca80da168c2
LeemaJosephine <leemajosephine3@gmail.com>
2026-06-30 23:54:08 +0530

    Complete VPC module

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -34,7 +34,9 @@ resource "aws_subnet" "public" {
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "${var.environment}-public-${count.index + 1}"
+  Name = "${var.environment}-public-${count.index + 1}"
+
+  "kubernetes.io/role/elb" = "1"
   }
 }
 
@@ -49,7 +51,9 @@ resource "aws_subnet" "private" {
   availability_zone = var.availability_zones[count.index]
 
   tags = {
-    Name = "${var.environment}-private-${count.index + 1}"
+  Name = "${var.environment}-private-${count.index + 1}"
+
+  "kubernetes.io/role/internal-elb" = "1"
   }
 }
 
